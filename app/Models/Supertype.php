bd88ac5633f567e4cdf50f5a515f7e95f957d1bf
Coycoding <fluffytoycoy@gmail.com>
2020-12-03 16:24:54 -0600

    removed trait duplication

--- app/Models/Supertype.php
+++ app/Models/Supertype.php
@@ -13,8 +13,6 @@ class Supertype extends NameOnlyModel
     protected $guarded = ['id'];
     protected $hidden = ['pivot', 'created_at', 'updated_at'];
 
-    use HasFactory;
-
     public function cards()
     {
       return $this->belongsToMany('App\Models\Card', 'card_supertypes');
